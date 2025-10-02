e73897e0786d515d4796aef98801eee2af71722d
maitesuropp <surop.maite@gmail.com>
2025-10-02 11:51:57 -0300

    Delete TP2/SUBCONSULTA 3.sql

--- TP2/SUBCONSULTA 3.sql
+++
@@ -1,4 +0,0 @@
-USE tp2g4;
-/*3 ¿Cuántas salidas ha realizado el barco con matrícula 'ABC123'?*/
-			SELECT Barcos.matricula, COUNT(id_salida) AS total_salida FROM Salidas, Barcos
-				WHERE Barcos.matricula = "ABC123";
